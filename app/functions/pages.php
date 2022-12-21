125da434ee251698b88577b7d6501d251ccba3ac
João Vítor <77066846+joaovitorsh@users.noreply.github.com>
2022-12-21 09:38:57 -0300

    first commit

---
+++ app/functions/pages.php
@@ -0,0 +1,23 @@
+<?php
+
+
+/**
+ * @throws Exception
+ */
+
+
+function load(): string
+{
+    if(isset($_GET['page'])){
+        $page = strip_tags($_GET['page']);
+        $page = "pages/{$page}.php";
+    } else {
+        $page = 'pages/home.php';
+    }
+
+    if(!file_exists($page)){
+        throw new Exception("Error Processing Request");
+    }
+
+    return $page;
+}
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/../app/functions/pages.php';?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css">
+
+    <title>Curso de PHP devclass</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <?php require load(); ?>
+    </div>
+</body>
+
+</html>
